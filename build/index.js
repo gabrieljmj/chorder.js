f65593a14325ffa25925acde757231d167314db3
Gabriel Jacinto <gamjj74@hotmail.com>
2019-02-02 16:34:08 -0200

    Add build for minified version

--- build/index.js
+++ build/index.js
@@ -4,36 +4,47 @@ const { rollup } = require('rollup')
 const babel = require('rollup-plugin-babel')
 const { uglify } = require('rollup-plugin-uglify')
 
-const options = {
+const getOptions = ({ minify = false } = {}) => {
+  const options = {
     input: {
-        input: 'src/chorder.js',
-        plugins: [
-            babel({
-                exclude: 'node_modules/**',
-                presets: [
-                    '@babel/preset-env'
-                ]
-            })
-        ]
+      input: 'src/chorder.js',
+      plugins: [
+          babel({
+              exclude: 'node_modules/**',
+              presets: [
+                  '@babel/preset-env'
+              ]
+          })
+      ]
     },
     output: {
-        file: 'dist/chorder.js',
-        format: 'cjs'
+      file: 'dist/chorder.js',
+      format: 'cjs'
     }
+  }
+
+  if (minify) {
+    options.output.file = 'dist/chorder.min.js'
+    options.input.plugins.push(uglify())
+  }
+
+  return options
 }
 
-process.env.env === 'prod' ? (options.input.plugins.push(uglify())) : null
-
 async function build() {
-    const bundle = await rollup(options.input)
+  const { input, output } = getOptions()
+  const { input: minifiedInput, output: minifiedOutput } = getOptions({minify: true})
 
-    await bundle.write(options.output)
+  const bundle = await rollup(input)
+  const minifiedBundle = await rollup(minifiedInput)
+
+  await bundle.write(output)
+  await minifiedBundle.write(minifiedOutput)
 }
 
 (async () => {
-    await build()
+  await build()
 
-    console.log(`-----------------
-Built!
-Dist: ${options.output.file}`)
+  console.log(`-----------------
+Built!`)
 })()
--- dist/chorder.js
+++ dist/chorder.js
@@ -1,1 +1,139 @@
-"use strict";function _classCallCheck(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}function _defineProperties(e,t){for(var a=0;a<t.length;a++){var r=t[a];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}function _createClass(e,t,a){return t&&_defineProperties(e.prototype,t),a&&_defineProperties(e,a),e}var FormTemplate=function(){function a(e,t){_classCallCheck(this,a),this.name=e,this._cloneFromTemplate(t)}return _createClass(a,[{key:"build",value:function(e){var a=this,t=this.template.cloneNode(!0);return t.dataset[this.name]=e,Array.from(t.querySelectorAll("*[cd-field]")).forEach(function(e){var t=e.dataset.cdName;e.setAttribute("name","".concat(a.name,"[").concat(t,"][]"))}),t.querySelector("button[cd-remove]").onclick=function(e){t.outerHTML=""},t}},{key:"_cloneFromTemplate",value:function(e){if(!e.hasAttribute("cd-template"))throw new Error("Invalid template. Add the 'cd-template' parameter.");this.template=e.cloneNode(!0)}}]),a}();function deleteElement(e){e.outerHTML=""}var Chorder=function(){function e(){_classCallCheck(this,e)}return _createClass(e,[{key:"init",value:function(){var o=this;Array.from(document.querySelectorAll("div[cd]")).forEach(function(e){var t=e.getAttribute("cd"),a=e.querySelector("div[cd-template]"),r=new FormTemplate(t,a),n=e.dataset.initialNumber||1;o.__createInitials(e,r,n),o.__applyEvents(e,r),deleteElement(a)})}},{key:"appendToData",value:function(e,t){e.querySelector("*[cd-data]").append(t.build())}},{key:"getData",value:function(e){var a=[],t=document.querySelector('*[cd="'+e+'"]');return Array.from(t.querySelectorAll("*[data-"+e+"]")).forEach(function(e){var t={};Array.from(e.querySelectorAll("*[cd-field]")).forEach(function(e){t[e.dataset.cdName]=e.value}),a.push(t)}),a}},{key:"__createInitials",value:function(e,t,a){for(var r=1;r<=a;r++)this.appendToData(e,t)}},{key:"__applyEvents",value:function(e,t){var a=this;e.querySelector("*[cd-add]").onclick=function(){a.appendToData(e,t)}}}]),e}();module.exports=Chorder;
+'use strict';
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+var FormTemplate =
+/*#__PURE__*/
+function () {
+  function FormTemplate(name, template) {
+    _classCallCheck(this, FormTemplate);
+
+    this.name = name;
+
+    this._cloneFromTemplate(template);
+  }
+
+  _createClass(FormTemplate, [{
+    key: "build",
+    value: function build(n) {
+      var _this = this;
+
+      var clone = this.template.cloneNode(true);
+      clone.dataset[this.name] = n;
+      Array.from(clone.querySelectorAll('*[cd-field]')).forEach(function (field) {
+        var name = field.dataset.cdName;
+        field.setAttribute('name', "".concat(_this.name, "[").concat(name, "][]"));
+      });
+
+      clone.querySelector('button[cd-remove]').onclick = function (e) {
+        clone.outerHTML = '';
+      };
+
+      return clone;
+    }
+  }, {
+    key: "_cloneFromTemplate",
+    value: function _cloneFromTemplate(template) {
+      if (!template.hasAttribute('cd-template')) {
+        throw new Error('Invalid template. Add the \'cd-template\' parameter.');
+      }
+
+      this.template = template.cloneNode(true);
+    }
+  }]);
+
+  return FormTemplate;
+}();
+
+function deleteElement(element) {
+  element.outerHTML = '';
+}
+
+var Chorder =
+/*#__PURE__*/
+function () {
+  function Chorder() {
+    _classCallCheck(this, Chorder);
+  }
+
+  _createClass(Chorder, [{
+    key: "init",
+    value: function init() {
+      var _this = this;
+
+      Array.from(document.querySelectorAll('div[cd]')).forEach(function (cd) {
+        var name = cd.getAttribute('cd');
+        var originalTemplateEl = cd.querySelector('div[cd-template]');
+        var template = new FormTemplate(name, originalTemplateEl);
+        var number = cd.dataset.initialNumber || 1;
+
+        _this.__createInitials(cd, template, number);
+
+        _this.__applyEvents(cd, template);
+
+        deleteElement(originalTemplateEl);
+      });
+    }
+  }, {
+    key: "appendToData",
+    value: function appendToData(chorderElement, templateElement) {
+      chorderElement.querySelector('*[cd-data]').append(templateElement.build());
+    }
+  }, {
+    key: "getData",
+    value: function getData(name) {
+      var data = [];
+      var cd = document.querySelector('*[cd="' + name + '"]');
+      Array.from(cd.querySelectorAll('*[data-' + name + ']')).forEach(function (el) {
+        var _data = {};
+        Array.from(el.querySelectorAll('*[cd-field]')).forEach(function (field) {
+          _data[field.dataset.cdName] = field.value;
+        });
+        data.push(_data);
+      });
+      return data;
+    }
+  }, {
+    key: "__createInitials",
+    value: function __createInitials(cd, template, n) {
+      for (var i = 1; i <= n; i++) {
+        this.appendToData(cd, template);
+      }
+    }
+  }, {
+    key: "__applyEvents",
+    value: function __applyEvents(cd, template) {
+      var _this2 = this;
+
+      var addBtn = cd.querySelector('*[cd-add]');
+
+      addBtn.onclick = function () {
+        _this2.appendToData(cd, template);
+      };
+    }
+  }]);
+
+  return Chorder;
+}();
+
+module.exports = Chorder;
